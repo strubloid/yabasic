97e55cd27e2b9d924a5e6b30fee3ad494d8c2c42
strubloid <strubloid@gmail.com>
2022-08-22 18:11:18 +0100

    changing the base structure

---
+++ src/components/test/component.ts
@@ -0,0 +1,5 @@
+export default class Test {
+  public getMessage = () => {
+    return 'TESSST';
+  };
+}
---
+++ src/yabasic.ts
@@ -0,0 +1,5 @@
+import Test from 'components/test/component';
+
+export const Greeter = (name: string) => `Hello ${name}`;
+
+export default Test;
